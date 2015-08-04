d16a917d0b72e7fbbac0defaed9ee5b14cbd6135
afriant1 <amandafriant@gmail.com>
2015-08-03 19:04:59 -0500

    Completes Part 2

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,46 @@
+class DirectorsController < ApplicationController
+  def index
+    @list_of_directors = Director.all
+  end
+
+  def show
+    @director = Director.find(params["id"])
+  end
+
+  def new_form
+    render("new_form.html.erb")
+  end
+
+  def create_row
+    d = Director.new
+    d.name = params["the_name"]
+    d.bio = params["the_bio"]
+    d.dob = params["the_dob"]
+    d.image_url = params["image_url"]
+    d.save
+    redirect_to("http://localhost:3000/directors")
+  end
+
+  def destroy
+    @director = Director.find(params["id"])
+    @director.destroy
+    render("destroy.html.erb")
+  end
+
+  def edit_form
+    @director = Director.find(params["id"])
+    render("edit_form.html.erb")
+  end
+
+  def update_row
+      d = Director.find(params["id"])
+      d.name = params["the_name"]
+      d.bio = params["the_bio"]
+      d.dob = params["the_dob"]
+      d.image_url = params["image_url"]
+      d.save
+      redirect_to("http://localhost:3000/directors")
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,20 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get("/", { :controller => "directors", :action => "index" })
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # Routes to CREATE directors
+  get("/directors/new", { :controller => "directors", :action => "new_form" })
+  get("/create_director", { :controller => "directors", :action => "create_row" })
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  # Routes to READ directors
+  get("/directors",           { :controller => "directors", :action => "index" })
+  get("/directors/:id",       { :controller => "directors", :action => "show" })
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  # Route to DELETE directors
+  get("/delete_director/:id", { :controller => "directors", :action => "destroy" })
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # Routes to EDIT directors
+  get("/directors/:id/edit",           { :controller => "directors", :action => "edit_form" })
+  get("/update_director/:id",           { :controller => "directors", :action => "update_row" })
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
